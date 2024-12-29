44685e201e2f69d94a9e64308a53de610d83bff5
Alwurts <jota780@gmail.com>
2024-12-29 18:14:03 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<main className="bg-gray-100 min-h-screen">
+			<h1 className="text-2xl font-bold">simple-shadcn-cli-demo</h1>
+		</main>
+	);
+}
+
+export default App;
