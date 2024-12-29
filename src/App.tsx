a7b2ce03f3db5806cae2beb192ff95faa8f8ac9e
Alwurts <jota780@gmail.com>
2024-12-29 19:05:56 +0100

    UPdatee examplee

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { AnimatedBadge } from "./components/ui/animated-badge";
+import { Separator } from "./components/ui/separator";
 
 function App() {
 	return (
 		<main className="bg-gray-100 min-h-screen flex items-center justify-center">
-			<div className="w-[500px] mx-auto flex flex-col gap-4">
+			<div className="w-[500px] mx-auto flex flex-col gap-4 p-4">
 				<div className="flex flex-col gap-1">
 					<h1 className="text-2xl font-bold">simple-shadcn-cli-demo</h1>
 					<p className="text-sm text-muted-foreground">
@@ -12,6 +13,26 @@ function App() {
 					</p>
 				</div>
 				<AnimatedBadge delay={0.2}>Hello World!</AnimatedBadge>
+				<a
+					href="/registry/animated-badge.json"
+					rel="noreferrer"
+					target="_blank"
+					className="text-sm text-muted-foreground hover:underline"
+				>
+					See the registry output
+				</a>
+				<Separator />
+				<div className="flex flex-col gap-1">
+					<h2 className="text-lg font-bold">See the simple-shadcn-cli at </h2>
+					<a
+						href="https://github.com/Alwurts/simple-shadcn-cli"
+						rel="noreferrer"
+						target="_blank"
+						className="text-sm text-muted-foreground hover:underline"
+					>
+						github.com/Alwurts/simple-shadcn-cli
+					</a>
+				</div>
 			</div>
 		</main>
 	);
